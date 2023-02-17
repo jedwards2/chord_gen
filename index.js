b668dc1ad5bb918e03f59df759420b4e8b1460e8
jedwards2 <46580372+jedwards2@users.noreply.github.com>
2023-02-17 17:48:28 -0500

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Note_1 = require("../theory/src/Note");
-var PitchClass_1 = require("../theory/src/PitchClass");
-var note = new Note_1["default"](new PitchClass_1["default"](4));
-note.printNote();
