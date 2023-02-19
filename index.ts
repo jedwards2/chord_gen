6f40fc440c6a33ed9dac1881809f11d1703c6e59
Jack <jack.morgan.edwards@gmail.com>
2023-02-18 21:45:52 -0500

    getNextChord plan

--- index.ts
+++ index.ts
@@ -1,5 +1,27 @@
 import Note from "../theory/src/Note"
 import PitchClass from "../theory/src/PitchClass";
 
+
+//inputRow will have structure 
+//[
+//{
+//  prob: 0.2;
+//  chord: ChordType
+//},
+//{
+// prob: 0.8,
+// chord: ChordType
+//},
+//]
+function getNextChord(inputRow) {
+  let seed = Math.random();
+  for (let i = 0; i < inputRow.length; i++) {
+    if (seed < inputRow[i].prob) {
+      return inputRow[i].chord;
+    }
+  }
+}
+
 let note = new Note(new PitchClass(4));
-note.printNote()
+note.printNote();
+
