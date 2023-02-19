b08a60c83dcbc47a52d468d21b4a60a244dc5cc4
Jack <jack.morgan.edwards@gmail.com>
2023-02-18 21:58:47 -0500

    matrix types

--- index.ts
+++ index.ts
@@ -1,19 +1,16 @@
 import Note from "../theory/src/Note"
 import PitchClass from "../theory/src/PitchClass";
+// import MajorTriad from "../theory/src/chords/MajorTriad";
 
+let matrix = {
+  I: [{ prob: 0.5, chord: "IV" }, { prob: 0.5, chord: "V" }],
+  IV: [{ prob: 0.9, chord: "V" }, { prob: 0.1, chord: "I" }],
+  V: [{ prob: 0.0, chord: "IV" }, { prob: 1.0, chord: "I" }],
+}
+type MatrixBlock = { prob: number, chord: String };
+type MatrixRow = MatrixBlock[];
 
-//inputRow will have structure 
-//[
-//{
-//  prob: 0.2;
-//  chord: ChordType
-//},
-//{
-// prob: 0.8,
-// chord: ChordType
-//},
-//]
-function getNextChord(inputRow) {
+function getNextChord(inputRow: MatrixRow) {
   let seed = Math.random();
   for (let i = 0; i < inputRow.length; i++) {
     if (seed < inputRow[i].prob) {
@@ -24,4 +21,5 @@ function getNextChord(inputRow) {
 
 let note = new Note(new PitchClass(4));
 note.printNote();
-
+console.log(matrix.I)
+getNextChord(matrix.I)
